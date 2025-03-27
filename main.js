0e3cae1985189e374fbea8ac71d951d541160fa7
msarlo <mateussarlo1@outlook.com>
2025-03-27 15:22:38 -0300

    tirando comentários inuteis

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import Grafo from "./Grafo.js";
 import Aestrela from "./Aestrela.js";
 
-// Criar o grafo de Kanto
+
 const grafo = new Grafo();
 grafo.criarGrafoKanto();
 
-// Definir os pontos de início e destino
+
 const inicio = "Pallet";
 const destino = "Indigo Plateau";
 
-// Executar o algoritmo A*
+
 const caminho = AEstrela.encontrarCaminho(grafo, inicio, destino);
 
-// Exibir o resultado
+
 console.log(`Caminho encontrado de ${inicio} até ${destino}:`, caminho.join(" -> "));
